9a16d00e2cbfef503e289619bf8cb392bb0091a3
Rajan Kumar <43785231+kumarrajan4264@users.noreply.github.com>
2021-01-02 22:12:16 +0530

    Update settings.py

--- django_school/django_school/settings.py
+++ django_school/django_school/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'd$pxg6fisc4iwzk&vz^s_d0lkf&k63l5a8f!obktw!jg#4zvp3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','aaptcoder.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','appcoder.herokuapp.com']
 
 
 # Application definition
